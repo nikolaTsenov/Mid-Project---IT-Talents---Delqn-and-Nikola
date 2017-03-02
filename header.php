343afd37c2e1719bb53269c7081cea90d0bfaee1
nikolaTsenov <niki_tsenov@abv.bg>
2017-03-02 07:54:28 +0200

    a little modification of the header

--- header.php
+++ header.php
@@ -2,9 +2,9 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<meta name="description" content="LayoutTask1"/>
-<meta http-equiv="author" name="Nikola Tsenov" />
-<title>7gag</title>
+<meta name="description" content="7cheers"/>
+<meta http-equiv="author" name="Nikola Tsenov, Delqn Kolev" />
+<title>7cheers</title>
 <link rel="stylesheet" href="./assets/css/reset.css" type="text/css" />
 <link rel="stylesheet" href="./assets/font-awesome-4.7.0/css/font-awesome.min.css"/>
 <link rel="stylesheet" href="./assets/css/gornaLenta.css" type="text/css" />
@@ -16,32 +16,40 @@
 				<div class="dropdown">
 					<!-- The inside of the button is unclickable, tried many tags, nothing worked. I liked my animation, so I just couldn't erase it...-->
 					<button onclick="dropdownFunction()" class="dropbtn"><!--  <p id="doIT">-->&#9776;<!-- &nbsp;<i class="fa fa-arrow-left" aria-hidden="true"></i></p>--></button>
-					<h2><a href="">Register</a></h2>
+					<h2><a href="">Home </a></h2>
   					<div id="myDropdown" class="dropdown-content">
-   						<a href="#mostPopular">ABOUT</a>
-   						<a href="#mostPopular">MOST POPULAR</a>
-   						<a href="#news">NEWS</a>
+   						<a href="#mostPopular">VIDEOS</a>
+   						<a href="#mostPopular">IMAGES</a>
+   						<a href="#news">GIFS</a>
+   						<a href="#contact">NEWS</a>
+   						<a href="?page=register">Register</a>
+   						<a href="?page=login">Login</a>
    						<a href="#contact">CONTACTS</a>
   					</div>
 				</div>
 				<form id="searchingForm">
   					<input type="text" name="search" placeholder="Search..">
 				</form>
-				<h1 id="heading">MOVIES</h1>
+				<h1 id="heading"><a href="?page=homepage"><img src="./assets/images/logo7.png" alt="logo7" />cheers</a></h1>
 				<ul id="topperNav">
-					<li id="register"><a href="?page=register">Register</a></li>
-					<li class="about"><a href="">ABOUT</a>
+					<li id="videos"><a href="">VIDEOS</a></li>
+					<li id="images"><a href="">IMAGES</a></li>
+					<li id="gifs"><a href="">GIFS</a></li>
+					<li class="news"><a href="">NEWS</a>
 						<div class="dropboxHover">
 							<a href="">Link 1</a>
 							<a href="">Link 2</a>
 							<a href="">Link 3</a>
 						</div>
 					</li>
-					<li id="Login"><a href="?page=login">Login</a></li>
-					<li id="news"><a href="">NEWS</a></li>
+					<li id="register"><a href="?page=register">Register</a></li>
+					<li id="login"><a href="?page=login">Login</a></li>
 					<li id="contacts"><a href="">CONTACTS</a></li>
 					<li id="search"><a href=""><i class="fa fa-search"></i></a></li>
 				</ul>
+				<form id="searchingForm2">
+  					<input type="text" name="search" placeholder="Search..">
+				</form>
 			</header>	
 	</div>	
 	<script type="text/javascript">
