4b44574fbc0926fad1a8cb1c4dbbc9ef178e352d
nikolaTsenov <niki_tsenov@abv.bg>
2017-03-15 14:12:57 +0200

    uploadScript.js-little bug fixed, now you can directly click from title to Upload

--- assets/javascript/scriptUpload.js
+++ assets/javascript/scriptUpload.js
@@ -28,24 +28,34 @@ function Validate(oForm) {
 
 var titleUpload = document.getElementById("fileTitle");
 var uploadValidTitle = true;
-document.getElementById("uploadSubmit").disabled = true;
+var submitButton = document.getElementById("uploadSubmit");
+
 titleUpload.onblur = function () {
 	if (titleUpload.value.trim().length < 3) {
 		uploadValidTitle = false;
 		document.getElementById("appendTitleWarning").innerHTML = "Your title must consist of at least 3 characters!";
+		submitButton.disabled = true;
 	} else if(titleUpload.value.trim().length > 30) {
 		uploadValidTitle = false;
 		document.getElementById("appendTitleWarning").innerHTML = "Your title cannot consist of more than 30 characters!";
+		submitButton.disabled = true;
 	} else {
 		uploadValidTitle = true;
 		document.getElementById("appendTitleWarning").innerHTML = "";
-		document.getElementById("uploadSubmit").disabled = false;
+		submitButton.disabled = false;
 	}
 }
 
-titleUpload.onkeypress = function () {
+
+titleUpload.onkeyup = function () {
 	if (!uploadValidTitle) {
 		document.getElementById("appendTitleWarning").innerHTML = "";
+	}
+	if (titleUpload.value.trim().length < 3) {
+		submitButton.disabled = true;
+	}
+	if (titleUpload.value.trim().length >= 3) {
+		submitButton.disabled = false;
 	}
 }
 
